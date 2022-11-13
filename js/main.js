c393a006a2ff285c39876b276b07aa58b8e3db15
mauritsf <57948944+mauritsf15@users.noreply.github.com>
2022-11-13 17:20:47 +0100

    Added comments

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 const articles = document.querySelectorAll('article');
 const resetBtn = document.querySelector('.reset-btn');
 
-let board = [       // visual board for computer
+let board = [           // visual board for computer
     ['', '', ''],
     ['', '', ''],
     ['', '', '']
 ];
 
-let hboard = [       // horizontal board for computer
+let hboard = [          // horizontal board for computer
     ['', '', ''],
     ['', '', ''],
     ['', '', '']
@@ -15,32 +15,36 @@ let hboard = [       // horizontal board for computer
 
 let gameActive = true;
 
+// For every possible three-in-a-row row
+// there is a value that increases when an X has been placed there
 //            H1 H2 H3   V1 V2 V3   D1 D2
 let xboard = [[0, 0, 0], [0, 0, 0], [0, 0]];
 
-let turns = 0;
+let turns = 0;  // counts how many turns there have been
 
-resetBtn.addEventListener('click', resetGame);
+resetBtn.addEventListener('click', resetGame);  // resets all the buttons
 
-for (let i = 0; i < articles.length; i++) {         // passes data of clicked object
-    articles[i].addEventListener('click', onClick);  // so it knows which place its been clicked
+for (let i = 0; i < articles.length; i++) {             // passes data of clicked object
+    articles[i].addEventListener('click', onClick);     // so it knows which place its been clicked
 }
 
-const dataBoard = [ // data board for special values
+const dataBoard = [     // data board for special values
     [articles[0], articles[3], articles[6]],
     [articles[1], articles[4], articles[7]],
     [articles[2], articles[5], articles[8]]
 ];
 
+// On click of article
 function onClick(data) {
-    let row = data.target.parentElement.className.charAt(4);
+    let row = data.target.parentElement.className.charAt(4);    // finds the path to the row in class name
     let col = data.target.parentElement.className.charAt(10);
     if (board[row][col] == '') {
         data.target.attributes[0].nodeValue = "img/cross.png";
         board[row][col] = 'x';
         hboard[col][row] = 'x';
-        xboard[0][row]++;
-        xboard[1][col]++;
+        // For the AI to recognize where there are 2 x's in a row or column (or diagonal)
+        xboard[0][row]++;   // Horizontal
+        xboard[1][col]++;   // Vertical
         if (`${row}${col}` == '11') {
             xboard[2][0]++;
             xboard[2][1]++;
@@ -50,11 +54,12 @@ function onClick(data) {
             xboard[2][0]++;
         }
         checkWin('x');
-        aiTurn();
+        aiTurn();   // Give turn to the AI!
     }
     turns++;
 }
 
+// Resets the board and variables
 function resetGame() {
     for (let i = 0; i < articles.length; i++) {
         articles[i].children[0].attributes[0].nodeValue = "img/trans.png";   // reset bg image to nothing
@@ -75,13 +80,10 @@ function resetGame() {
     gameActive = true;
 }
 
+// THE MAGIC! My first artificial intelligence ;)
 function aiTurn() {
-    let placed = false;
-    if (turns == 8) {
-        placed = true;
-        turns++;
-        return;
-    }
+    let placed = false;     // Check if the AI has already placed in this turn
+    // Puts the 'o' in the middle if the player hasn't placed it in the middle yet
     if (board[1][1] == '') {
         if (placed == false) {
             placeAI(1,1);
@@ -90,13 +92,14 @@ function aiTurn() {
             return;
         }
     }
+    // Checks for double x's in every three-in-a-row row
     for (let i = 0; i < xboard.length; i++) {
         for (let j = 0; j < xboard[i].length; j++) {
-            if (xboard[i][j] == 2) {
-                if (i == 0) {
-                    if (board[j].includes('o') == false) {
-                        for (k = 0; k < board[j].length; k++) {
-                            if (board[j][k] == '') {
+            if (xboard[i][j] == 2) {    // If it finds two x's,
+                if (i == 0) {           // and (in this case) the row is horizontal (for the computer at least)
+                    if (board[j].includes('o') == false) {      // Does the row contain an 'o' yet?
+                        for (k = 0; k < board[j].length; k++) { // If not, it finds the empty place!
+                            if (board[j][k] == '') {            // and puts an 'o' there.
                                 if (placed == false) {
                                     placeAI(j, k);
                                     placed = true;
@@ -106,7 +109,7 @@ function aiTurn() {
                             }
                         }
                     }
-                } else if (i == 1) {
+                } else if (i == 1) {                            // The same is done for the visual rows.
                     if (hboard[j].includes('o') == false) {
                         for (k = 0; k < board[j].length; k++) {
                             if (hboard[j][k] == '') {
@@ -119,9 +122,9 @@ function aiTurn() {
                             }
                         }
                     }
-                } else if (i == 2) {
-                    if (j == 0) {
-                        if (board[2][2] == '') {
+                } else if (i == 2) {                // Now to the diagonal rows.
+                    if (j == 0) {                   // Since there are only two possible diagonals, it's just as easy
+                        if (board[2][2] == '') {    // to write the exact coordinates instead of using loops.
                             if (placed == false) {
                                 placeAI(2, 2);
                                 placed = true;
@@ -157,21 +160,21 @@ function aiTurn() {
             }
         }
     }
-    if (placed == false) {
-        let g = Math.floor(Math.random() * 3)
-        let h = Math.floor(Math.random() * 3)
-        if (board[g][h] == '') {
+    if (placed == false) {  // Is there nothing placed yet? This happens when there are no rows with 2 x's.
+        let g = Math.floor(Math.random() * 3)   // Chooses a random number between 1-3
+        let h = Math.floor(Math.random() * 3)   // And, of course, here too.
+        if (board[g][h] == '') {        // If it's empty, place it!
             if (placed == false) {
                 placeAI(g, h)
                 placed = true
                 turns++;
                 return;
             }
-        } else if (turns > 7) {
+        } else if (turns > 7) {         // Is it not empty? Check if the game is still running :p
             placed = true
         } else if (gameActive == false) {
             placed = true
-        } else {
+        } else {                        // Not empty and game is still on? While loop!
             while (placed == false) {
                 console.log('hi')
                 g = Math.floor(Math.random() * 3)
@@ -189,6 +192,7 @@ function aiTurn() {
     }
 }
 
+// Short function to get the AI to place an O somewhere
 function placeAI(row, col) {
     document.querySelector(`.row-${row}.col-${col}`).children[0].attributes[0].nodeValue = "img/circle.png"
     board[row][col] = 'o';
@@ -196,6 +200,8 @@ function placeAI(row, col) {
     checkWin('o')
 }
 
+// Did someone win yet? Every turn, this is checked for respectably 'x' and 'o',
+// whichever gets mentioned when calling the function.
 function checkWin(player) {
     if (turns == 8) {
         stopGame();
@@ -227,6 +233,7 @@ function checkWin(player) {
     }
 }
 
+// STOP THE GAME!!!
 function stopGame() {       // changes visual board to everything filled
     board = [               // so nothing can be added
         ['n', 'n', 'n'],
