83ce25c97e859f3851c01d5066953e5ecff96631
mauritsf <57948944+mauritsf15@users.noreply.github.com>
2022-11-13 16:45:40 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,75 +1,189 @@
-const articles = document.querySelectorAll('article')
-const resetBtn = document.querySelector('.reset-btn')
+const articles = document.querySelectorAll('article');
+const resetBtn = document.querySelector('.reset-btn');
 
 let board = [       // visual board for computer
     ['', '', ''],
     ['', '', ''],
     ['', '', '']
-]
+];
 
-resetBtn.addEventListener('click', resetGame)
+let hboard = [       // horizontal board for computer
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+];
+
+let gameActive = true;
+
+//            H1 H2 H3   V1 V2 V3   D1 D2
+let xboard = [[0, 0, 0], [0, 0, 0], [0, 0]];
+
+let turns = 0;
+
+resetBtn.addEventListener('click', resetGame);
 
 for (let i = 0; i < articles.length; i++) {         // passes data of clicked object
-    articles[i].addEventListener('click', onClick)  // so it knows which place its been clicked
+    articles[i].addEventListener('click', onClick);  // so it knows which place its been clicked
 }
 
 const dataBoard = [ // data board for special values
     [articles[0], articles[3], articles[6]],
     [articles[1], articles[4], articles[7]],
     [articles[2], articles[5], articles[8]]
-]
+];
 
 function onClick(data) {
-    let row = data.path[1].classList[0].charAt(4)
-    let col = data.path[1].classList[1].charAt(4)
+    let row = data.target.parentElement.className.charAt(4);
+    let col = data.target.parentElement.className.charAt(10);
     if (board[row][col] == '') {
-        data.target.attributes[0].nodeValue = "img/cross.png"
-        board[row][col] = 'x'
-        checkWin('x')
-        aiTurn()
+        data.target.attributes[0].nodeValue = "img/cross.png";
+        board[row][col] = 'x';
+        hboard[col][row] = 'x';
+        xboard[0][row]++;
+        xboard[1][col]++;
+        if (`${row}${col}` == '11') {
+            xboard[2][0]++;
+            xboard[2][1]++;
+        } else if (`${row}${col}` == '20' || `${row}${col}` == '02') {
+            xboard[2][1]++;
+        } else if (`${row}${col}` == '00' || `${row}${col}` == '22') {
+            xboard[2][0]++;
+        }
+        checkWin('x');
+        aiTurn();
     }
+    turns++;
 }
 
 function resetGame() {
     for (let i = 0; i < articles.length; i++) {
-        articles[i].children[0].attributes[0].nodeValue = "img/trans.png"   // reset bg image to nothing
-        articles[i].style.backgroundColor = ''
-        turn = true
-        board = [
-            ['', '', ''],
-            ['', '', ''],
-            ['', '', '']
-        ]
+        articles[i].children[0].attributes[0].nodeValue = "img/trans.png";   // reset bg image to nothing
+        articles[i].style.backgroundColor = '';
     }
+    board = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ];
+    hboard = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ];
+    xboard = [[0, 0, 0], [0, 0, 0], [0, 0]];
+    turns = 0;
+    gameActive = true;
 }
 
 function aiTurn() {
-    if (board[1][1] == '') {
-        placeAI(1,1);
+    let placed = false;
+    if (turns == 8) {
+        placed = true;
+        turns++;
         return;
     }
-    // check if two x's are in one row
-    let count = 0;
-    let totalcount = 0;
-    let empty = [];
-    for (let vertical = 0; vertical < 3; vertical++) {
-        for (let horizontal = 0; horizontal < 3; horizontal++) {
-            if (board[vertical][horizontal] == '') {
-                empty.push(`${vertical}`)
-                empty.push(`${horizontal + 1}`)
-            }
-            totalcount++;
-            if (board[vertical][horizontal] == 'x') {
-                count++;
-                if (count == 2) {
-                    placeAI(empty[1], empty[0])
-                    return;
+    if (board[1][1] == '') {
+        if (placed == false) {
+            placeAI(1,1);
+            placed = true;
+            turns++;
+            return;
+        }
+    }
+    for (let i = 0; i < xboard.length; i++) {
+        for (let j = 0; j < xboard[i].length; j++) {
+            if (xboard[i][j] == 2) {
+                if (i == 0) {
+                    if (board[j].includes('o') == false) {
+                        for (k = 0; k < board[j].length; k++) {
+                            if (board[j][k] == '') {
+                                if (placed == false) {
+                                    placeAI(j, k);
+                                    placed = true;
+                                    turns++;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                } else if (i == 1) {
+                    if (hboard[j].includes('o') == false) {
+                        for (k = 0; k < board[j].length; k++) {
+                            if (hboard[j][k] == '') {
+                                if (placed == false) {
+                                    placeAI(k, j);
+                                    placed = true;
+                                    turns++;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                } else if (i == 2) {
+                    if (j == 0) {
+                        if (board[2][2] == '') {
+                            if (placed == false) {
+                                placeAI(2, 2);
+                                placed = true;
+                                turns++;
+                                break;
+                            }
+                        } else if (board[0][0] == '') {
+                            if (placed == false) {
+                                placeAI(0, 0);
+                                placed = true;
+                                turns++;
+                                break;
+                            }
+                        }
+                    } else {
+                        if (board[0][2] == '') {
+                            if (placed == false) {
+                                placeAI(0, 2);
+                                placed = true;
+                                turns++;
+                                break;
+                            }
+                        } else if (board[2][0] == '') {
+                            if (placed == false) {
+                                placeAI(2, 0);
+                                placed = true;
+                                turns++;
+                                break;
+                            }
+                        }
+                    }
                 }
-            if (totalcount == 3) {
-                count = 0;
-                totalcount = 0;
-                empty = []
             }
+        }
+    }
+    if (placed == false) {
+        let g = Math.floor(Math.random() * 3)
+        let h = Math.floor(Math.random() * 3)
+        if (board[g][h] == '') {
+            if (placed == false) {
+                placeAI(g, h)
+                placed = true
+                turns++;
+                return;
+            }
+        } else if (turns > 7) {
+            placed = true
+        } else if (gameActive == false) {
+            placed = true
+        } else {
+            while (placed == false) {
+                console.log('hi')
+                g = Math.floor(Math.random() * 3)
+                h = Math.floor(Math.random() * 3)
+                if (board[g][h] == '') {
+                    if (placed == false) {
+                        placeAI(g, h)
+                        placed = true
+                        turns++;
+                        return;
+                    }
+                }
             }
         }
     }
@@ -77,10 +191,16 @@ function aiTurn() {
 
 function placeAI(row, col) {
     document.querySelector(`.row-${row}.col-${col}`).children[0].attributes[0].nodeValue = "img/circle.png"
-    board[row][col] = 'o'
+    board[row][col] = 'o';
+    hboard[col][row] = 'o';
+    checkWin('o')
 }
 
 function checkWin(player) {
+    if (turns == 8) {
+        stopGame();
+        return;
+    }
     for (let i = 0; i <= 2; i++) {
         if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {          // vertical check
             dataBoard[i][0].style.backgroundColor = '#42f563'
@@ -113,4 +233,5 @@ function stopGame() {       // changes visual board to everything filled
         ['n', 'n', 'n'],
         ['n', 'n', 'n']
     ]
+    gameActive = false;
 }
